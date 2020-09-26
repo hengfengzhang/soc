e2fee06cc7474204704f57ad746b2292558edf97
AllenZhang <allenzhang>
2020-09-26 17:38:58 +0800

    initial

---
+++ test/test_temp.v
@@ -0,0 +1,14 @@
+module test_temp #(parameter aa=1) (
+  input clk, //input clk
+  input rst_n,
+  input [31:0] data_in,
+  output done
+);
+
+wire test;
+wire test1;
+
+assign test = test1;
+
+endmodule
+
