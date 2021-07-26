1ea3778659ae51f81881c7392bba969338e2262c
hengfeng.zhang <hengfengzhang@foxmail.com>
2021-07-26 11:27:45 +0800

    Add files via upload

--- design/mmc_ss/scr/gen_reg.py
+++ design/mmc_ss/scr/gen_reg.py
@@ -75,7 +75,7 @@ def hw_wr_data(reg_name):
     return string
 
 def hw_wr_wen(reg_name):
-    string = ' ' + reg_name + '_hw_wen '
+    string = '' + reg_name + '_hw_wen'
     return string
 
 def gen_hw_wr_block(reg_name, hw_wr):
@@ -93,8 +93,16 @@ def gen_hw_rd_assign(reg_name, hw_wr):
         string = None
     return string
 
+def sw_wr_en():
+    string = 'penable && pwrite && psel'
+    return string
+
+def sw_addr_en(offset_address):
+    string = '(paddr_d==' + offset_address + ')'
+    return string
+
 def sw_wr_cond(offset_address):
-    string = '( ( paddr_d == ' + offset_address + ' ) && penable && pwrite && psel )'
+    string = '( ' + sw_addr_en(offset_address) + ' && ' + sw_wr_en() + ' )'
     return string
 
 def gen_sw_wr_block(reg_name, sw_wr, offset_address):
@@ -136,11 +144,10 @@ def gen_sw_rd_always(reg_mux):
     return string
 
 def gen_pready_assign(reg_mux):
-    rdat = ''
+    rdat = '\n\t\t\t\t\t'
     for rdata_mux in reg_mux:
-        rdat = rdat + '&&' + rdata_mux
-    string = '''
-\nassign\tpready\t=\t~(\t''' + gen_sw_rd_en() + '\t' + rdat.strip('\n') + '\t);'
+        rdat = rdat + '\n\t\t\t\t\t| ' + rdata_mux
+    string = '\n\nassign\tpready\t=\t~( ' + sw_wr_en() + ' & (' + '1\'h0' +  rdat.strip('\n') + ')\t);'
     return string
 
 
@@ -159,73 +166,92 @@ def get_reg_def_dict(reg_def_list):
         reg_def_dict.update({reg_name:reg_attri})
     return reg_def_dict
 
+def gen_reg_rtl(output_file, base_address, reg_def_dict):
+    inst_head = []
+    inst_body = []
+    inst_tail = []
+
+    design_name = output_file.replace('.v', '')
+    module_begin = 'module ' + design_name
+    port_begin = '(  //port begin'
+    port_end = '); //port end'
+    module_end = '\nendmodule'
+
+    # generate header
+    inst_head.append(module_begin)
+    inst_head.append(port_begin)
+    for reg_name in reg_def_dict.keys():
+        offset_address, reg_bit, reg_default, sw_wr, sw_rd, hw_wr, hw_rd = reg_def_dict[reg_name]
+        if hw_wr == 'Y':
+            port_line = '\n\tinput ' + gen_bitwise(reg_bit) + ' ' + hw_wr_data(reg_name) + ', '
+            inst_head.append(port_line)
+            port_line = '\n\tinput ' + '\t\t\t' + ' ' + hw_wr_wen(reg_name) + ', '
+            inst_head.append(port_line)
+        if hw_rd == 'Y':
+            port_line = '\n\toutput ' + gen_bitwise(reg_bit) + ' ' + hw_rd_data(reg_name) + ', '
+            inst_head.append(port_line)
+    inst_head.append(sw_access_port())
+    inst_head.append(port_end)
+
+    # generate body
+    reg_mux = []
+    hw_wr_list = []
+    inst_body.append(gen_offset_addr_assign(base_address))
+    inst_body.append(gen_paddr_d_block(base_address))
+    for reg_name in reg_def_dict.keys():
+        offset_address, reg_bit, reg_default, sw_wr, sw_rd, hw_wr, hw_rd = reg_def_dict[reg_name]
+
+        inst_body.append(gen_reg_def(reg_name, reg_bit))
+
+        inst_body.append(gen_clk_always())
+        inst_body.append(gen_rst_block(reg_name, reg_default))
+        if 'Y' in hw_wr:
+            inst_body.append(gen_hw_wr_block(reg_name, hw_wr))
+            hw_wr_list.append('(' + sw_addr_en(offset_address) + '&&' + hw_wr_wen(reg_name) + ')')
+        if 'Y' in sw_wr:
+            inst_body.append(gen_sw_wr_block(reg_name, sw_wr, offset_address))
+        if 'Y' in hw_rd:
+            inst_body.append(gen_hw_rd_assign(reg_name, hw_rd))
+        if 'Y' in sw_rd:
+            reg_mux.append(' ( ' + reg_name + ' & { ' + str(reg_bit) + '{(offset_addr==' + offset_address + ')} } )\n')
+
+
+    inst_body.append(gen_sw_rd_always(reg_mux))
+
+    inst_body.append(gen_pready_assign(hw_wr_list))
+
+    inst_body.append(module_end)
+
+    with open(output_file, 'w+') as w_obj:
+        inst_rtl = gen_rtl(inst_head, inst_body, inst_tail)
+        for line in inst_rtl:
+            print(line)
+            w_obj.write(str(line))
+
+def gen_reg_model(output_file, base_address, reg_def_dict):
+    print('''
+//TBD
+uvm_block_begin
+    uvm_map_begin
+        uvm_map_define
+    uvm_map_end    
+    uvm_reg_begin
+        uvm_field_begin
+            uvm_field_define
+        uvm_field_end
+    uvm_reg_end   
+uvm_block_end
+     ''')
+
 if __name__ == '__main__':
     input_file  = []
     output_file = []
     input_file, output_file = get_opt()
     base_address, reg_def_list = get_excel(input_file)
     reg_def_dict = get_reg_def_dict(reg_def_list)
+    gen_reg_rtl(output_file, base_address, reg_def_dict)
+    gen_reg_model(output_file, base_address, reg_def_dict)
 
-    inst_head = []
-    inst_body = []
-    inst_tail = []
-
-    design_name  = output_file.replace('.v', '')
-    module_begin = 'module ' + design_name
-    port_begin   = '(  //port begin'
-    port_end     = '); //port end'
-    module_end   = '\nendmodule'
-
-    #generate header
-    inst_head.append(module_begin)
-    inst_head.append(port_begin)
-    for reg_name in reg_def_dict.keys():
-        offset_address, reg_bit, reg_default, sw_wr, sw_rd, hw_wr, hw_rd =  reg_def_dict[reg_name]
-        if hw_wr == 'Y':
-            port_line = '\n\tinput ' + gen_bitwise(reg_bit) + ' ' + hw_wr_data(reg_name) + ', '
-            inst_head.append(port_line)
-            port_line = '\n\tinput ' + '\t\t\t' + ' ' + hw_wr_wen(reg_name ) + ', '
-            inst_head.append(port_line)
-        if hw_rd == 'Y':
-            port_line = '\n\toutput ' + gen_bitwise(reg_bit) + ' ' + hw_rd_data(reg_name ) + ', '
-            inst_head.append(port_line)
-    inst_head.append(sw_access_port())
-    inst_head.append(port_end)
-
-   #generate body
-    reg_mux = []
-    hw_wr_list = []
-    inst_body.append( gen_offset_addr_assign(base_address) )
-    inst_body.append( gen_paddr_d_block(base_address) )
-    for reg_name in reg_def_dict.keys():
-        offset_address, reg_bit, reg_default, sw_wr, sw_rd, hw_wr, hw_rd = reg_def_dict[reg_name]
-        reg_mux.append(' ( ' + reg_name + ' & { ' + str(reg_bit) + '{(offset_addr==' + offset_address + ')} } )\n')
-
-        inst_body.append( gen_reg_def(reg_name, reg_bit) )
-
-        inst_body.append( gen_clk_always() )
-        inst_body.append( gen_rst_block(reg_name, reg_default) )
-        if 'Y' in hw_wr:
-            inst_body.append( gen_hw_wr_block(reg_name, hw_wr) )
-            hw_wr_list.append( hw_wr_wen(reg_name) )
-        if 'Y' in sw_wr:
-            inst_body.append( gen_sw_wr_block(reg_name, sw_wr, offset_address) )
-
-        if 'Y' in hw_rd:
-            inst_body.append(gen_hw_rd_assign(reg_name, hw_rd))
-
-    inst_body.append( gen_sw_rd_always(reg_mux) )
-
-    inst_body.append( gen_pready_assign(hw_wr_list) )
-
-
-    inst_body.append( module_end )
-
-    with open(output_file, 'w+') as w_obj:
-        inst_rtl = gen_rtl(inst_head, inst_body, inst_tail)
-        for line in inst_rtl:
-            print(line)
-            w_obj.write( str(line) )
 
 
 
